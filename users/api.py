d717c81e51378647f67850c0adc25e8d8814ee72
Ramón Albertí <krainet@gmail.com>
2015-07-25 14:49:14 +0200

    Using APIView

--- users/api.py
+++ users/api.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from users.serializers import UserSerializer
-# importo json render
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 __author__ = 'hadock'
-from django.views.generic import View
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from users.serializers import UserSerializer
 
-class UserListAPI(View):
+
+
+class UserListAPI(APIView):
     def get(self,req):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         serialized_users = serializer.data # en data van los datos...
-        renderer = JSONRenderer()
-        json_users = renderer.render(serialized_users)
-        return HttpResponse(json_users)
+        return Response(serialized_users)

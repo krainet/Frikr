1c64219e7ff628212297bf87aec6ceedf2649534
Ramón Albertí <krainet@gmail.com>
2015-07-25 18:30:54 +0200

    API rest con vista generica Photo Acabada

--- photos/api.py
+++ photos/api.py
@@ -2,9 +2,14 @@
 from photos.serializers import PhotoSerializer
 __author__ = 'hadock'
 from photos.models import Photo
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 
 class PhotoListAPI(ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
